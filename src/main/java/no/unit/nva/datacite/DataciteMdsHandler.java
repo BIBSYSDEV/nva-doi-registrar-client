f9489b4f7a0d44ad4c1128ac6e62d2a2f14c83fa
nr <nr@unit.no>
2020-02-18 09:44:47 +0100

    initial commit

---
+++ src/main/java/no/unit/nva/datacite/DataciteMdsHandler.java
@@ -0,0 +1,4 @@
+package no.unit.nva.datacite;
+
+public class DataciteMdsHandler {
+}
---
+++ src/test/java/no/unit/nva/datacite/DataciteMdsHandlerTest.java
@@ -0,0 +1,12 @@
+package no.unit.nva.datacite;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class DataciteMdsHandlerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
