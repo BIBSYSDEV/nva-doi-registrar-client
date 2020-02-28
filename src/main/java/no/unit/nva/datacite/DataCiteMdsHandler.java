a6bdd85af52d33f4b0229bb155e21deac749b29d
nr <nr@unit.no>
2020-02-28 15:53:38 +0100

    WIP

--- src/main/java/no/unit/nva/datacite/DataCiteMdsHandler.java
+++ src/main/java/no/unit/nva/datacite/DataCiteMdsHandler.java
@@ -1,10 +1,21 @@
 package no.unit.nva.datacite;
 
 import com.amazonaws.secretsmanager.caching.SecretCache;
+import com.amazonaws.services.lambda.model.InvocationType;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
-import no.unit.nva.datacite.model.generated.Resource;
+import no.unit.nva.datacite.model.generated.*;
+import no.unit.nva.model.Publication;
+import no.unit.nva.model.Publisher;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.utils.URIBuilder;
@@ -17,9 +28,19 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.InvokeRequest;
+import com.amazonaws.services.lambda.model.InvokeResult;
+import com.amazonaws.services.lambda.model.ServiceException;
+import org.zalando.problem.ProblemModule;
+
 
 /**
  * Handler for requests to Lambda function.
@@ -41,9 +62,9 @@ public class DataCiteMdsHandler implements RequestHandler<Map<String, Object>, G
     public static final String ERROR_SETTING_DOI_URL = "Error setting DOI url";
     public static final String ERROR_DELETING_DOI_METADATA = "Error deleting DOI metadata";
     public static final String ERROR_CREATING_LANDING_PAGE_URL = "Error creating landing page url";
+    public static final String ERROR_CREATING_DATACITE_XML = "Error creating Datacite XML";
 
     public static final String HTTPS = "https";
-    public static final String CHARSET_UTF8 = "UTF-8";
     public static final String PARENTHESES_START = "(";
     public static final String PARENTHESES_STOP = ")";
     public static final String WHITESPACE = " ";
@@ -53,8 +74,12 @@ public class DataCiteMdsHandler implements RequestHandler<Map<String, Object>, G
     private transient SecretCache secretCache = new SecretCache();
     private transient DataCiteMdsConnection dataCiteMdsConnection;
 
-    public DataCiteMdsHandler() {
+    private transient PublicationConverter publicationConverter = new PublicationConverter();
 
+    private final transient ObjectMapper objectMapper = createObjectMapper();
+    private final transient ObjectMapper xmlMapper = createXmlMapper();
+
+    public DataCiteMdsHandler() {
     }
 
     public DataCiteMdsHandler(DataCiteMdsConnection dataCiteMdsConnection, SecretCache secretCache) {
@@ -91,11 +116,11 @@ public class DataCiteMdsHandler implements RequestHandler<Map<String, Object>, G
             return gatewayResponse;
         }
 
-        // TODO: Retrieve resource metadata in datacite format directly
-        //  or transform and validate metadata for datacite here
-        // Resource resource;
-        // String identifier;
-        String institution = "unit"; // Institution from resource
+        String identifier = ((Map<String, String>) input.get(PATH_PARAMETERS_KEY)).get(PATH_PARAMETER_IDENTIFIER_KEY);
+
+        Publication publication = retrievePublication(identifier);
+
+        String institution = publication.getPublisher().getIdentifier().toString(); // Institution from resource
 
         // Check if resource institution is present in datacite configs
         if (!dataCiteMdsClientConfigsMap.containsKey(institution)) {
@@ -110,8 +135,6 @@ public class DataCiteMdsHandler implements RequestHandler<Map<String, Object>, G
                 dataCiteMdsClientConfig.getDataCiteMdsClientUsername(),
                 dataCiteMdsClientConfig.getDataCiteMdsClientPassword());
 
-        String identifier = ((Map<String, String>) input.get(PATH_PARAMETERS_KEY)).get(PATH_PARAMETER_IDENTIFIER_KEY);
-
         // To fully register a DOI there are two steps: post metadata and post doi (url)
         String url;
         try {
@@ -123,23 +146,34 @@ public class DataCiteMdsHandler implements RequestHandler<Map<String, Object>, G
             return gatewayResponse;
         }
 
-        Resource resource = new Resource(); // Resource fetched using identifier;
+        Resource resource = publicationConverter.toResource(publication);
+        String dataciteXml;
+        try {
+            dataciteXml = new XmlMapper()
+                    .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
+                    .writeValueAsString(resource);
+        } catch (JsonProcessingException e) {
+            gatewayResponse.setErrorBody(ERROR_CREATING_DATACITE_XML);
+            gatewayResponse.setStatusCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+            return gatewayResponse;
+        }
 
-        return createDoi(gatewayResponse, dataCiteMdsClientConfig, url, resource);
+        return createDoi(gatewayResponse, dataCiteMdsClientConfig, url, dataciteXml);
     }
 
     private GatewayResponse createDoi(GatewayResponse gatewayResponse, DataCiteMdsClientConfig dataCiteMdsClientConfig,
-                                      String url, Resource resource) {
+                                      String url, String dataciteXml) {
         String createdDoi;
         try (CloseableHttpResponse createMetadataResponse =
-                     dataCiteMdsConnection.postMetadata(dataCiteMdsClientConfig.getInstitutionPrefix(), resource)) {
+                     dataCiteMdsConnection.postMetadata(dataCiteMdsClientConfig.getInstitutionPrefix(), dataciteXml)) {
             if (createMetadataResponse.getStatusLine().getStatusCode() != Response.Status.CREATED.getStatusCode()) {
                 gatewayResponse.setErrorBody(ERROR_SETTING_DOI_METADATA + WHITESPACE + PARENTHESES_START
                         + createMetadataResponse.getStatusLine().getStatusCode() + PARENTHESES_STOP);
                 gatewayResponse.setStatusCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
                 return gatewayResponse;
             }
-            String createMetadataResponseBody = EntityUtils.toString(createMetadataResponse.getEntity(), CHARSET_UTF8);
+            String createMetadataResponseBody = EntityUtils.toString(createMetadataResponse.getEntity(),
+                    StandardCharsets.UTF_8.name());
             createdDoi = StringUtils.substringBetween(createMetadataResponseBody, PARENTHESES_START, PARENTHESES_STOP);
         } catch (IOException | URISyntaxException e) {
             gatewayResponse.setErrorBody(ERROR_SETTING_DOI_METADATA);
@@ -217,4 +251,61 @@ public class DataCiteMdsHandler implements RequestHandler<Map<String, Object>, G
         }
     }
 
+    protected Publication retrievePublication(String identifier) {
+
+//        InvokeRequest invokeRequest = new InvokeRequest()
+//                .withInvocationType(InvocationType.RequestResponse)
+//                .withFunctionName(Config.getInstance().getNvaFetchResourceFunctionName())
+//                .withPayload("{\"httpMethod\" : \"GET\",\"pathParameters\" : {\"identifier\" : \"" + identifier +
+//                        "\"}}");
+//
+//        try {
+//            AWSLambda awsLambda = AWSLambdaClientBuilder.defaultClient();
+//            InvokeResult invokeResult = awsLambda.invoke(invokeRequest);
+//
+//            if (invokeResult.getStatusCode() == Response.Status.OK.getStatusCode()) {
+//
+//                try {
+//                    String result = new String(invokeResult.getPayload().array(), StandardCharsets.UTF_8);
+//                    JsonNode event = objectMapper.readTree(result);
+//                    Iterator<JsonNode> elements = event.get("body").get("Items").elements();
+//                    String json = objectMapper.writeValueAsString(elements.next());
+//
+//                    Publication publication = objectMapper.readValue(json, Publication.class);
+//                    return publication;
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//        } catch (ServiceException e) {
+//            System.out.println(e);
+//        }
+        return null;
+    }
+
+    protected String createDataciteResourceXml(Publication publication) throws MalformedURLException, JsonProcessingException {
+        Resource resource = publicationConverter.toResource(publication);
+        return xmlMapper.writeValueAsString(resource);
+    }
+
+    /**
+     * Create ObjectMapper.
+     *
+     * @return objectMapper
+     */
+    public static ObjectMapper createObjectMapper() {
+        return new ObjectMapper()
+                .registerModule(new ProblemModule())
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+    }
+
+    private ObjectMapper createXmlMapper() {
+        return new XmlMapper().setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    }
 }
