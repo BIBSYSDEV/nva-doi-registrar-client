4f9fe83d64d7a1236574a6dc784aebd10a4450c5
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-10-15 00:21:08 +0200

    NP-1688: test for AwsEventBridgeEventTest

--- dynamo-event-to-datacite-request/src/test/java/no/unit/nva/events/models/AwsEventBridgeEventTest.java
+++ dynamo-event-to-datacite-request/src/test/java/no/unit/nva/events/models/AwsEventBridgeEventTest.java
@@ -1,5 +1,41 @@
 package no.unit.nva.events.models;
 
+import static no.unit.nva.hamcrest.DoesNotHaveNullOrEmptyFields.doesNotHaveNullOrEmptyFields;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.nio.file.Path;
+import no.unit.nva.events.examples.DataciteDoiRequest;
+import nva.commons.utils.IoUtils;
+import nva.commons.utils.JsonUtils;
+import org.junit.jupiter.api.Test;
+
 public class AwsEventBridgeEventTest {
 
+    private final String detailJson = IoUtils.stringFromResources(Path.of("validEventBridgeEvent.json"));
+
+    @Test
+    public void objectMapperReturnsAwsEverBridgeDetailObjectForValidJson() throws JsonProcessingException {
+        var event = parseEvent();
+        assertThat(event, is(not(nullValue())));
+        assertThat(event, doesNotHaveNullOrEmptyFields());
+    }
+
+    @Test
+    public void objectMapperSerialized() throws JsonProcessingException {
+        var event = parseEvent();
+        assertThat(event, is(not(nullValue())));
+        assertThat(event, doesNotHaveNullOrEmptyFields());
+    }
+
+    private AwsEventBridgeEvent<AwsEventBridgeDetail<DataciteDoiRequest>> parseEvent()
+        throws JsonProcessingException {
+        TypeReference<AwsEventBridgeEvent<AwsEventBridgeDetail<DataciteDoiRequest>>> detailTypeReference =
+            new TypeReference<>() {};
+        return JsonUtils.objectMapper.readValue(detailJson, detailTypeReference);
+    }
 }
