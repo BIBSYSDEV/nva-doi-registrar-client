cab868ca5eeeb5e540c0d0a8bb7e7338645a4f79
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-10-22 09:47:28 +0200

    NP-1668: javadoc for WithType class

--- event-handler-template/src/test/java/no/unit/nva/events/handlers/WithType.java
+++ event-handler-template/src/test/java/no/unit/nva/events/handlers/WithType.java
@@ -1,5 +1,9 @@
 package no.unit.nva.events.handlers;
 
+/**
+ * Interface for adding type to Json objects. The interface fixes the problem of Jackson where the annotations include
+ * the type only for the top object and not for the children object.
+ */
 public interface WithType {
 
     default String getType() {
