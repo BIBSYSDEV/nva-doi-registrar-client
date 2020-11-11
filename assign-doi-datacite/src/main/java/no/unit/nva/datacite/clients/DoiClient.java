07d9192ab9d06c61f934c86ca947dca0b9e8ca73
Roy Sindre Norangshol <rono@unit.no>
2020-11-11 09:37:44 +0100

    Update javadoc for DoiClient

--- assign-doi-datacite/src/main/java/no/unit/nva/datacite/clients/DoiClient.java
+++ assign-doi-datacite/src/main/java/no/unit/nva/datacite/clients/DoiClient.java
@@ -6,16 +6,23 @@ import no.unit.nva.datacite.clients.models.Doi;
 
 /**
  * NVAs supported DOI operations.
+ *
+ * <p>DOIs can exist in three states: draft, registered, and findable. DOIs are in the draft state when metadata
+ * have been registered, and will transition to the findable state when registering a landing page (URL).
+ *
+ * @see <a href="https://support.datacite.org/docs/landing-pages">Best practices for landing pages</a>
+ * @see <a href="https://support.datacite.org/docs/tombstone-pages">Best practices for Tombstone Landing pages</a>
+ * @see <a href="https://support.datacite.org/docs/versioning"Best practices about versioning</a>
  */
 public interface DoiClient {
 
     /**
-     * Create a DOI where registry agency (ie datacite) auto generates the suffix under the associated customer
-     * repository for NVA.
+     * Create a DOI where registry agency (ie datacite) auto generates the suffix under the associated repository for
+     * NVA connected to the customer.
      *
      * @param customerId          NVAs customerId
      * @param metadataDataciteXml datacite schema serialized xml as string
-     * @return DOI from provider in the following syntax: prefix/suffix
+     * @return {@link Doi} containing prefix/suffix ({@link Doi#toIdentifier()}) from provider
      * @throws ClientException Error while communicating with Registry Agency
      */
     Doi createDoi(String customerId, String metadataDataciteXml) throws ClientException;
@@ -24,35 +31,39 @@ public interface DoiClient {
      * Update metadata for a given DOI.
      *
      * @param customerId          NVAs customerId
-     * @param doi                 DOI in the format of prefix/suffix
      * @param metadataDataciteXml datacite schema serialized xml as string
+     * @param doi                 {@link Doi} containing prefix/suffix ({@link Doi#toIdentifier()})
      * @throws ClientException Error while communicating with Registry Agency
+     * @see <a href="https://support.datacite.org/docs/metadata-quality">Metadata quality requirements</a>
+     * @see <a href="https://support.datacite.org/docs/connecting-research-outputs">Connect Reseearch Outputs
+     *     (SCHOLIX)</a>
      */
     void updateMetadata(String customerId, Doi doi, String metadataDataciteXml) throws ClientException;
 
     /**
-     * Request to mark DOI as findable.
+     * Set landing page for a Doi,  this also turns the DOI into findable state!
      *
      * <p>Once a DOI is findable, it cannot be deleted!
      *
      * @param customerId NVAs customerId
-     * @param doi        DOI in the format of prefix/suffix
      * @param url        Location of landing page.
+     * @param doi        {@link Doi} containing prefix/suffix ({@link Doi#toIdentifier()})
      * @throws ClientException Error while communicating with Registry Agency
+     * @see <a href="https://support.datacite.org/docs/datacite-doi-display-guidelines">DOI display guidelines.</a>
      */
     void setLandingPage(String customerId, Doi doi, URI url) throws ClientException;
 
     /**
      * Delete metadata from DOI.
      *
-     * <p>DOIs can exist in three states: draft, registered, and findable. DOIs are in the draft state when metadata
-     * have been registered, and will transition to the findable state when registering a URL.
-     *
      * <p>Findable DOIs can be transitioned to the registered state (the metadata are no longer included in the search
-     * index) using this deleteMetadata command.
+     * index) using this ${@link #deleteMetadata(String, Doi)}.
+     *
+     * <p>Notice: Updating metadata for a DOI that has been in deleted state (registered) will turn it into findable
+     * state.
      *
      * @param customerId NVAs customerId
-     * @param doi        DOI in the format of prefix/suffix
+     * @param doi        {@link Doi} containing prefix/suffix ({@link Doi#toIdentifier()})
      * @throws ClientException Error while communicating with Registry Agency
      */
     void deleteMetadata(String customerId, Doi doi) throws ClientException;
@@ -61,8 +72,9 @@ public interface DoiClient {
      * Delete a DOI which is in draft.
      *
      * <p>The DOI cannot be deleted after its first promotion to findable state.
+     *
      * @param customerId NVAs customerId
-     * @param doi DOI in the format of prefix/suffix
+     * @param doi        {@link Doi} containing prefix/suffix ({@link Doi#toIdentifier()})
      * @throws ClientException Error while communicating with Registry Agency
      */
     void deleteDraftDoi(String customerId, Doi doi) throws ClientException;
