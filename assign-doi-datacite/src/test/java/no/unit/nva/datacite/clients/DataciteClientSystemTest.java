f6c8d317d0e8271a3aa97467896e17169363f6ec
Roy Sindre Norangshol <rono@unit.no>
2020-11-11 13:31:01 +0100

    Checkstyle_OverloadMethodsDeclarationOrder fix?

--- assign-doi-datacite/src/test/java/no/unit/nva/datacite/clients/DataciteClientSystemTest.java
+++ assign-doi-datacite/src/test/java/no/unit/nva/datacite/clients/DataciteClientSystemTest.java
@@ -298,38 +298,42 @@ class DataciteClientSystemTest extends DataciteClientTestBase {
         return new X509ExtendedTrustManager() {
 
             @Override
-            public void checkClientTrusted(X509Certificate[] x509Certificates, String s, Socket socket)
-                throws CertificateException {
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
             }
 
             @Override
-            public void checkServerTrusted(X509Certificate[] x509Certificates, String s, Socket socket)
-                throws CertificateException {
-            }
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 
-            @Override
-            public void checkClientTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine)
-                throws CertificateException {
-            }
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine)
-                throws CertificateException {
-            }
-
-            @Override
-            public void checkClientTrusted(final X509Certificate[] x509Certificates, final String authType)
-                throws CertificateException {
-            }
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] x509Certificates, String authType)
-                throws CertificateException {
             }
 
             @Override
             public X509Certificate[] getAcceptedIssuers() {
                 return new X509Certificate[0];
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket)
+                throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket)
+                throws CertificateException {
+
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine)
+                throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine)
+                throws CertificateException {
+
             }
         };
     }
