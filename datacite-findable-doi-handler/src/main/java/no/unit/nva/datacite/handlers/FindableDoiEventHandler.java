3cef67bec893a00c2bf18420b85f3f67228ee780
Roy Sindre Norangshol <rono@unit.no>
2020-11-24 18:56:42 +0100

    Reorder fields

--- datacite-findable-doi-handler/src/main/java/no/unit/nva/datacite/handlers/FindableDoiEventHandler.java
+++ datacite-findable-doi-handler/src/main/java/no/unit/nva/datacite/handlers/FindableDoiEventHandler.java
@@ -33,18 +33,22 @@ import org.slf4j.LoggerFactory;
 
 public class FindableDoiEventHandler extends DestinationsEventBridgeEventHandler<PublicationHolder, DoiUpdateHolder> {
 
-    public static final String PUBLICATION_IS_MISSING_ERROR = "Publication is missing";
-    public static final String CUSTOMER_ID_IS_MISSING_ERROR = "CustomerId is missing";// log messages
-    public static final String RECEIVED_REQUEST_TO_MAKE_DOI_FINDABLE_LOG =
-        "Received request to set landing page (make findable) for DOI {} to landing page {} for {}";
     public static final String EVENT_SOURCE = "doi.updateDoiStatus";
+    // Data validation exceptions
+    public static final String PUBLICATION_IS_MISSING_ERROR = "Publication is missing";
+    public static final String CUSTOMER_ID_IS_MISSING_ERROR = "CustomerId is missing";
     public static final String DOI_IS_MISSING_OR_INVALID_ERROR = "Doi is missing or invalid";
     public static final String PUBLICATION_ID_MISSING_ERROR = "Publication id is missing";
     public static final String DOI_REQUEST_STATUS_WRONG_ERROR = "DoiRequestStatus is not APPROVED";
-    private static final Logger logger = LoggerFactory.getLogger(FindableDoiEventHandler.class);
-    private static final String SUCCESSFULLY_MADE_DOI_FINDABLE = "Successfully handled request for Doi {} : {}";
-    private static final String TRANSFORMING_PUBLICATION_ERROR = "Failed transforming publication into XML matching "
+    public static final String TRANSFORMING_PUBLICATION_ERROR = "Failed transforming publication into XML matching "
         + "DataCite XMLSchema";
+
+    // log errors
+    private static final String RECEIVED_REQUEST_TO_MAKE_DOI_FINDABLE_LOG =
+        "Received request to set landing page (make findable) for DOI {} to landing page {} for {}";
+    private static final String SUCCESSFULLY_MADE_DOI_FINDABLE = "Successfully handled request for Doi {} : {}";
+
+    private static final Logger logger = LoggerFactory.getLogger(FindableDoiEventHandler.class);
     private final DoiClient doiClient;
 
     @JacocoGenerated
