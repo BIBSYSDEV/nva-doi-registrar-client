2852e3aa9b021e4c6710212313b5c988f42f4f04
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-01-26 14:44:49 +0100

    NP-3566: linting

--- assign-doi-datacite/src/main/java/no/unit/nva/doi/datacite/clients/DataCiteClient.java
+++ assign-doi-datacite/src/main/java/no/unit/nva/doi/datacite/clients/DataCiteClient.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
-
 import no.unit.nva.doi.DoiClient;
 import no.unit.nva.doi.datacite.clients.exception.ClientException;
 import no.unit.nva.doi.datacite.clients.exception.CreateDoiException;
@@ -45,30 +44,30 @@ public class DataCiteClient implements DoiClient {
     public static final String PREFIX_TEMPLATE_ENTRY = "{}";
 
     public static final String DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES = COLON_SPACE
-        + PREFIX_TEMPLATE_ENTRY
-        + HTTP_STATUS_LOG_TEMPLATE;
+                                                                      + PREFIX_TEMPLATE_ENTRY
+                                                                      + HTTP_STATUS_LOG_TEMPLATE;
     public static final String ERROR_UPDATING_METADATA_FOR_DOI_TEMPLATE =
         ERROR_UPDATING_METADATA_FOR_DOI
-            + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
+        + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
     public static final String ERROR_DELETING_DOI_TEMPLATE =
         ERROR_DELETING_DOI
-            + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
+        + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
     public static final String ERROR_DELETING_DOI_METADATA_TEMPLATE =
         ERROR_DELETING_DOI_METADATA
-            + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
+        + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
     public static final String ERROR_SETTING_DOI_URL_TEMPLATE =
         ERROR_SETTING_DOI_URL
-            + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
+        + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
     public static final String ERROR_GETTING_DOI_TEMPLATE =
-            ERROR_GETTING_DOI
-            + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
+        ERROR_GETTING_DOI
+        + DOI_AND_HTTP_STATUS_TEMPLATE_ENTRIES;
 
     private static final String HTTP_FAILED_RESPONSE_MESSAGE = "{}";
     public static final String ERROR_CREATING_DOI_TEMPLATE =
         ERROR_CREATING_DOI
-            + PREFIX_TEMPLATE_ENTRY
-            + HTTP_STATUS_LOG_TEMPLATE
-            + HTTP_FAILED_RESPONSE_MESSAGE;
+        + PREFIX_TEMPLATE_ENTRY
+        + HTTP_STATUS_LOG_TEMPLATE
+        + HTTP_FAILED_RESPONSE_MESSAGE;
     private static final Logger logger = LoggerFactory.getLogger(DataCiteClient.class);
     private final DataCiteConnectionFactory dataCiteApiConnectionFactory;
     private final DataCiteConfigurationFactory configFactory;
@@ -99,7 +98,7 @@ public class DataCiteClient implements DoiClient {
             DataCiteRestConnection connection = prepareAuthenticatedDataCiteRestConnection(customerId);
             String doiPrefix = customerConfigInfo.getCustomerDoiPrefix();
             HttpResponse<String> response = sendDraftDoiRequest(connection, doiPrefix);
-            logger.info("Datacite response:"+response.body());
+            logger.info("Datacite response:" + response.body());
             DraftDoiDto responseBody = DraftDoiDto.fromJson(response.body());
 
             return responseBody.toDoi(doiProxy);
