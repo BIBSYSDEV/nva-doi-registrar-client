61e065b191d269e547ba8499a070d0748d172333
anetteOlli <anette.olli.siiri@sikt.no>
2023-03-03 15:13:04 +0100

    NP-37500-preparing-to-remove-publication-datamodel-from-DraftDoiHandler, implemented

--- datacite-draft-doi-handler/src/test/java/no/unit/nva/datacite/handlers/DraftDoiHandlerTest.java
+++ datacite-draft-doi-handler/src/test/java/no/unit/nva/datacite/handlers/DraftDoiHandlerTest.java
@@ -2,7 +2,8 @@ package no.unit.nva.datacite.handlers;
 
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
 import static no.unit.nva.datacite.handlers.DraftDoiHandler.CUSTOMER_ID_IS_MISSING_ERROR;
-import static no.unit.nva.datacite.handlers.DraftDoiHandler.PUBLICATION_IS_MISSING_ERROR;
+import static no.unit.nva.datacite.handlers.DraftDoiHandler.PUBLICATION_HAS_A_DOI_ALREADY;
+import static no.unit.nva.datacite.handlers.DraftDoiHandler.PUBLICATION_ID_IS_MISSING_ERROR;
 import static nva.commons.core.attempt.Try.attempt;
 import static nva.commons.core.ioutils.IoUtils.stringToStream;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,7 +21,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Path;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import no.unit.nva.datacite.commons.DoiUpdateEvent;
 import no.unit.nva.datacite.commons.DoiUpdateRequestEvent;
@@ -29,8 +29,6 @@ import no.unit.nva.doi.datacite.clients.exception.ClientException;
 import no.unit.nva.doi.datacite.clients.exception.CreateDoiException;
 import no.unit.nva.doi.models.Doi;
 import no.unit.nva.identifiers.SortableIdentifier;
-import no.unit.nva.model.Organization;
-import no.unit.nva.model.Publication;
 import nva.commons.core.ioutils.IoUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,7 +36,7 @@ import org.junit.jupiter.api.function.Executable;
 import org.mockito.invocation.InvocationOnMock;
 
 public class DraftDoiHandlerTest {
-    
+
     public static final String DOI_IDENTIFIER = "10.1052/identifier";
     public static final String EVENT_DETAIL_FIELD = "detail";
     public static final String DETAIL_RESPONSE_PAYLOAD_FIELD = "responsePayload";
@@ -47,13 +45,13 @@ public class DraftDoiHandlerTest {
     public static final int SAMPLE_STATUS_CODE = 500;
     public static final String PUBLICATION_IDENTIFIER_IN_RESOURCE_FILES =
         "017772f8ce52-4d10352d-7974-472e-be34-484c5f4f194d";
-    
+
     private DraftDoiHandler handler;
     private ByteArrayOutputStream outputStream;
     private Context context;
-    
+
     private AtomicReference<URI> inputBuffer;
-    
+
     @BeforeEach
     public void setUp() throws ClientException {
         var doiClient = doiClientReturningDoi();
@@ -62,87 +60,88 @@ public class DraftDoiHandlerTest {
         context = mock(Context.class);
         inputBuffer = new AtomicReference<>();
     }
-    
+
+    @Test
+    public void shouldThrowExceptionWhenDoiIsPresentInEvent() throws IOException {
+        try (InputStream inputStream = IoUtils.inputStreamFromResources(
+            "doi_publication_event_doi_present.json")) {
+            Executable action = () -> handler.handleRequest(inputStream, outputStream, context);
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, action);
+            assertThat(exception.getMessage(), is(PUBLICATION_HAS_A_DOI_ALREADY));
+        }
+    }
+
     @Test
     public void handleRequestReturnsDoiUpdateDtoWithPublicationUriWhenInputHasInstitutionOwner() {
-        InputStream inputStream = IoUtils.inputStreamFromResources(
-            Path.of("doi_publication_event_valid.json"));
+        InputStream inputStream = IoUtils.inputStreamFromResources("doi_publication_event_valid.json");
         handler.handleRequest(inputStream, outputStream, context);
-        
         DoiUpdateEvent response = parseResponse();
         SortableIdentifier actualPublicationIdentifier = response.getItem().getPublicationIdentifier();
         assertThat(actualPublicationIdentifier.toString(), is(PUBLICATION_IDENTIFIER_IN_RESOURCE_FILES));
     }
-    
+
     @Test
     public void handleRequestCallsDoiClientCreateDoiWhenInputHasInstitutionOwner() throws IOException {
         String inputString = IoUtils.stringFromResources(
             Path.of("doi_publication_event_valid.json"));
-        
-        URI expectedInputToDataCiteClient = extractExpectedPublisherIdFromEventBridgeEvent(inputString);
+
+        URI expectedInputToDataCiteClient = extractCustomerIdFromEventBridgeEvent(inputString);
         handler.handleRequest(stringToStream(inputString), outputStream, context);
         URI actualInputToDataCiteCient = inputBuffer.get();
         assertThat(actualInputToDataCiteCient, is(equalTo(expectedInputToDataCiteClient)));
     }
-    
+
     @Test
     public void handleRequestThrowsExceptionContainingTheCauseWhenDoiClientThrowsException()
         throws ClientException {
         String inputString = IoUtils.stringFromResources(Path.of("doi_publication_event_valid.json"));
-        
+
         DraftDoiHandler handlerReceivingException = new DraftDoiHandler(doiClientThrowingException());
         Executable action = () -> handlerReceivingException.handleRequest(stringToStream(inputString), outputStream,
-            context);
+                                                                          context);
         RuntimeException exception = assertThrows(RuntimeException.class, action);
         Throwable actualCause = exception.getCause();
         assertThat(actualCause.getMessage(), containsString(EXPECTED_ERROR_MESSAGE));
     }
-    
+
     @Test
-    public void handleRequestThrowsIllegalArgumentExceptionOnMissingEventItem() {
-        InputStream inputStream = IoUtils.inputStreamFromResources(
-            "doi_publication_event_empty_item.json");
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-            () -> handler.handleRequest(inputStream, outputStream,
-                context));
-        
-        assertThat(exception.getMessage(), is(PUBLICATION_IS_MISSING_ERROR));
+    public void handleRequestThrowsIllegalArgumentExceptionOnMissingEventItem() throws IOException {
+        try (InputStream inputStream = IoUtils.inputStreamFromResources(
+            "doi_publication_event_empty_item.json")) {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                                                              () -> handler.handleRequest(inputStream, outputStream,
+                                                                                          context));
+            assertThat(exception.getMessage(), is(PUBLICATION_ID_IS_MISSING_ERROR));
+        }
     }
-    
+
     @Test
-    public void handleRequestThrowsIllegalArgumentExceptionOnMissingCustomerId() {
-        InputStream inputStream = IoUtils.inputStreamFromResources(
-            "doi_publication_event_empty_institution_owner.json");
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-            () -> handler.handleRequest(inputStream, outputStream,
-                context));
-        
-        assertThat(exception.getMessage(), is(CUSTOMER_ID_IS_MISSING_ERROR));
+    public void handleRequestThrowsIllegalArgumentExceptionOnMissingCustomerId() throws IOException {
+        try (InputStream inputStream = IoUtils.inputStreamFromResources(
+            "doi_publication_event_empty_institution_owner.json")) {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                                                              () -> handler.handleRequest(inputStream, outputStream,
+                                                                                          context));
+
+            assertThat(exception.getMessage(), is(CUSTOMER_ID_IS_MISSING_ERROR));
+        }
     }
-    
-    private URI extractExpectedPublisherIdFromEventBridgeEvent(String inputString) throws IOException {
-        
+
+    private URI extractCustomerIdFromEventBridgeEvent(String inputString) throws IOException {
+
         JsonNode eventObject = dtoObjectMapper.readTree(inputString);
         JsonNode responsePayload = eventObject.path(EVENT_DETAIL_FIELD).path(DETAIL_RESPONSE_PAYLOAD_FIELD);
         DoiUpdateRequestEvent doiUpdateRequestEvent =
             dtoObjectMapper.convertValue(responsePayload, DoiUpdateRequestEvent.class);
-        
-        return getPublisher(doiUpdateRequestEvent);
+
+        return doiUpdateRequestEvent.getCustomerId();
     }
-    
-    private URI getPublisher(DoiUpdateRequestEvent doiUpdateRequestEvent) {
-        return Optional.of(doiUpdateRequestEvent)
-                   .map(DoiUpdateRequestEvent::getItem)
-                   .map(Publication::getPublisher)
-                   .map(Organization::getId)
-                   .orElse(null);
-    }
-    
+
     private DoiUpdateEvent parseResponse() {
         return attempt(() -> dtoObjectMapper.readValue(outputStream.toString(), DoiUpdateEvent.class))
                    .orElseThrow();
     }
-    
+
     private DoiClient doiClientReturningDoi() throws ClientException {
         DoiClient doiClient = mock(DoiClient.class);
         Doi doi = Doi.fromDoiIdentifier(DOI_IDENTIFIER);
@@ -150,13 +149,13 @@ public class DraftDoiHandlerTest {
             .thenAnswer(invocation -> saveInputAndReturnSampleDoi(doi, invocation));
         return doiClient;
     }
-    
+
     private Doi saveInputAndReturnSampleDoi(Doi doi, InvocationOnMock invocation) {
         URI customerId = invocation.getArgument(0);
         inputBuffer.set(customerId);
         return doi;
     }
-    
+
     private DoiClient doiClientThrowingException() throws ClientException {
         DoiClient doiClient = mock(DoiClient.class);
         when(doiClient.createDoi(any())).thenAnswer(invocation -> {
