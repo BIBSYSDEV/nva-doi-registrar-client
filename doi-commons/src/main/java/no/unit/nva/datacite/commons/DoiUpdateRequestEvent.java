636f875ab046159ef5de5f51d7ad269aaa161edd
anetteOlli <anette.olli.siiri@sikt.no>
2023-03-03 09:45:12 +0100

    NP-37500-preparing-to-remove-publication-datamodel-from-events, implemented

--- doi-commons/src/main/java/no/unit/nva/datacite/commons/DoiUpdateRequestEvent.java
+++ doi-commons/src/main/java/no/unit/nva/datacite/commons/DoiUpdateRequestEvent.java
@@ -2,6 +2,7 @@ package no.unit.nva.datacite.commons;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.net.URI;
 import java.util.Objects;
 import no.unit.nva.model.Publication;
 import nva.commons.core.JacocoGenerated;
@@ -13,22 +14,43 @@ public class DoiUpdateRequestEvent {
 
     @JsonProperty(TOPIC)
     private final String topic;
+
+    @Deprecated
     @JsonProperty(ITEM)
     private final Publication item;
+
+    @JsonProperty("doi")
+    private final URI doi;
+
+    @JsonProperty("publicationId")
+    private final URI publicationId;
+
+    @JsonProperty("customerId")
+    private final URI customerId;
 
     @JacocoGenerated
     @JsonCreator
     public DoiUpdateRequestEvent(
         @JsonProperty(TOPIC) String type,
-        @JsonProperty(ITEM) Publication publication) {
+        @JsonProperty(ITEM) Publication publication,
+        @JsonProperty("doi") URI doi,
+        @JsonProperty("publicationId") URI publicationId,
+        @JsonProperty("customerId") URI customerId) {
         this.topic = type;
         this.item = publication;
+        this.doi = doi;
+        this.publicationId = publicationId;
+        this.customerId = customerId;
     }
 
     @JacocoGenerated
     @Override
     public int hashCode() {
-        return Objects.hash(getTopic(), getItem());
+        return Objects.hash(getTopic(),
+                            getItem(),
+                            getDoi(),
+                            getPublicationId(),
+                            getCustomerId());
     }
 
     @JacocoGenerated
@@ -41,7 +63,11 @@ public class DoiUpdateRequestEvent {
             return false;
         }
         DoiUpdateRequestEvent that = (DoiUpdateRequestEvent) o;
-        return Objects.equals(getTopic(), that.getTopic()) && Objects.equals(getItem(), that.getItem());
+        return Objects.equals(getTopic(), that.getTopic())
+               && Objects.equals(getItem(), that.getItem())
+               && Objects.equals(getDoi(), that.getDoi())
+               && Objects.equals(getCustomerId(), that.getCustomerId())
+               && Objects.equals(getPublicationId(), that.getPublicationId());
     }
 
     @JacocoGenerated
@@ -52,5 +78,21 @@ public class DoiUpdateRequestEvent {
     @JacocoGenerated
     public Publication getItem() {
         return item;
+    }
+
+    @Deprecated
+    @JacocoGenerated
+    public URI getDoi() {
+        return doi;
+    }
+
+    @JacocoGenerated
+    public URI getPublicationId() {
+        return publicationId;
+    }
+
+    @JacocoGenerated
+    public URI getCustomerId() {
+        return customerId;
     }
 }
