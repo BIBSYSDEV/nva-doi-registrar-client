b914f552a7b2a6df9c21428d2a2a89a3484a8aa2
anetteOlli <anette.olli.siiri@sikt.no>
2023-03-22 15:48:47 +0100

    testing event invoke directly with sqs

--- datacite-findable-doi-handler/src/main/java/no/unit/nva/datacite/handlers/FindableDoiDLQHandler.java
+++ datacite-findable-doi-handler/src/main/java/no/unit/nva/datacite/handlers/FindableDoiDLQHandler.java
@@ -3,13 +3,16 @@ package no.unit.nva.datacite.handlers;
 import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
+import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse.BatchItemFailure;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import java.util.ArrayList;
 import no.unit.nva.commons.json.JsonUtils;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FindableDoiDLQHandler implements RequestHandler<SQSEvent, Void> {
+public class FindableDoiDLQHandler implements RequestHandler<SQSEvent, SQSBatchResponse> {
 
     private static final Logger logger = LoggerFactory.getLogger(FindableDoiDLQHandler.class);
 
@@ -19,9 +22,10 @@ public class FindableDoiDLQHandler implements RequestHandler<SQSEvent, Void> {
     }
 
     @Override
-    public Void handleRequest(SQSEvent input, Context context) {
+    public SQSBatchResponse handleRequest(SQSEvent input, Context context) {
+        var batchItemFailures = new ArrayList<BatchItemFailure>();
         logger.info("Received input: {}", getJsonString(input));
-        return null;
+        return new SQSBatchResponse(batchItemFailures);
     }
 
     private String getJsonString(SQSEvent input) {
