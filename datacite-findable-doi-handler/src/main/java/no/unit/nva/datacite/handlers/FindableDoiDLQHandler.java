4367d5b22b168c38b4a43394dfbd7d8fc1333d39
anetteOlli <anette.olli.siiri@sikt.no>
2023-03-22 14:11:47 +0100

    added logging

--- datacite-findable-doi-handler/src/main/java/no/unit/nva/datacite/handlers/FindableDoiDLQHandler.java
+++ datacite-findable-doi-handler/src/main/java/no/unit/nva/datacite/handlers/FindableDoiDLQHandler.java
@@ -1,8 +1,10 @@
 package no.unit.nva.datacite.handlers;
 
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import no.unit.nva.commons.json.JsonUtils;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +20,11 @@ public class FindableDoiDLQHandler implements RequestHandler<SQSEvent, Void> {
 
     @Override
     public Void handleRequest(SQSEvent input, Context context) {
-        logger.info("Received input: {}", input);
+        logger.info("Received input: {}", getJsonString(input));
         return null;
+    }
+
+    private String getJsonString(SQSEvent input) {
+        return attempt(() -> JsonUtils.dtoObjectMapper.writeValueAsString(input)).orElseThrow();
     }
 }
