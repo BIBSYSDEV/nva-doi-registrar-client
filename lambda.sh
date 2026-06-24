7016f41c1936647e145404c7703ebccdef158e43
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-06-24 10:58:24 +0200

    chore: Delete unused code

--- lambda.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
- aws lambda invoke --function-name orestis-stack-nva-datacite-mds-EventProducer-1P3OPXX6YR2JT \
-   --invocation-type Event   \
-   --payload file://requestPayload.json \
-   --cli-binary-format raw-in-base64-out \
-    response.json
